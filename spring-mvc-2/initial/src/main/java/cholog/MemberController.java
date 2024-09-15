82402008d4122cc13051c169f8648934a9a06a9f
QUWR <hnn06134@gmail.com>
2024-09-15 13:40:06 +0900

    message

---
+++ spring-mvc-2/initial/src/main/java/cholog/MemberController.java
@@ -0,0 +1,64 @@
+package cholog;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class MemberController {
+
+    private final List<Member> members = new ArrayList<>();
+    private final AtomicLong index = new AtomicLong(1);
+
+    @PostMapping("/members")
+    public ResponseEntity<Void> create(@RequestBody Member member) {
+
+        // TODO: member 정보를 받아서 생성한다.
+
+        Member newMember = Member.toEntity(member, index.getAndIncrement());
+        members.add(newMember);
+        return ResponseEntity.created(URI.create("/members/" + newMember.getId())).build();
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<Member>> read() {
+
+        // TODO: 저장된 모든 member 정보를 반환한다.
+
+        return ResponseEntity.ok().body(members);
+    }
+
+    @PutMapping("/members/{id}")
+    public ResponseEntity<Void> update(@RequestBody Member newMember, @PathVariable Long id) {
+
+        // TODO: member의 수정 정보와 url 상의 id 정보를 받아 member 정보를 수정한다.
+
+        Member member = members.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        member.update(newMember);
+        return null;
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        // TODO: url 상의 id 정보를 받아 member를 삭제한다.
+
+        Member member = members.stream()
+                .filter(it -> Objects.equals(it.getId(), id))
+                .findFirst()
+                .orElseThrow(RuntimeException::new);
+
+        members.remove(member);
+
+        return ResponseEntity.noContent().build();
+    }
+}
