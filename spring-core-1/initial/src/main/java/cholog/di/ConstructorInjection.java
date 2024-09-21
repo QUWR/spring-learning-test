6f839e67d09e139d96d5d9e0361d597d26e194c7
QUWR <hnn06134@gmail.com>
2024-09-21 21:07:07 +0900

    message

--- spring-core-1/initial/src/main/java/cholog/di/ConstructorInjection.java
+++ spring-core-1/initial/src/main/java/cholog/di/ConstructorInjection.java
@@ -1,5 +1,6 @@
 package cholog.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +10,12 @@ public class ConstructorInjection {
     /*
     ConstructorInjection으로 InjectionBean 주입받기
      */
+
+    @Autowired      //생략 가능
+    public ConstructorInjection(InjectionBean injectionBean) {
+        this.injectionBean = injectionBean;
+    }
+
     public String sayHello() {
         return injectionBean.hello();
     }
